f0d216420cb381002a355e6d362d512851960e4f
Felipe Cavani <fcavani@gmail.com>
2015-10-26 12:36:46 -0200

    stats funcs for int64

--- stats.go
+++ stats.go
@@ -46,3 +46,41 @@ func StdDevDuration(values []time.Duration) float64 {
 	}
 	return math.Sqrt((1.0 / float64(len(values)-1)) * sum)
 }
+
+func MinInt64(values []int64) int64 {
+	var min int64 = math.MaxInt64
+	for _, val := range values {
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
+
+func MaxInt64(values []int64) int64 {
+	var max int64 = math.MinInt64
+	for _, val := range values {
+		if val > max {
+			max = val
+		}
+	}
+	return max
+}
+
+func AvgInt64(values []int64) float64 {
+	var sum int64
+	for _, val := range values {
+		sum += val
+	}
+	return float64(sum) / float64(len(values))
+}
+
+func StdDevInt64(values []int64) float64 {
+	// StdDevDuration: amostral
+	mean := AvgInt64(values)
+	var sum float64
+	for _, val := range values {
+		sum += (float64(val) - mean) * (float64(val) - mean)
+	}
+	return math.Sqrt((1.0 / float64(len(values)-1)) * sum)
+}
